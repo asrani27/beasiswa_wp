e2cb2f80ff22be9576067ca841dbc84d400baef2
asrani <asrani.27@gmail.com>
2025-01-21 11:16:38 +0800

    f

--- resources/views/admin/laporan/pdf_hasilbkm.blade.php
+++ resources/views/admin/laporan/pdf_hasilbkm.blade.php
@@ -13,11 +13,12 @@
     <table width="100%">
         <tr>
             {{-- <td width="15%">
-                <img src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path('logo/dprd.png'))) }}" width="80px"> &nbsp;&nbsp;
+                <img src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path('logo/dprd.png'))) }}"
+                    width="80px"> &nbsp;&nbsp;
             </td> --}}
             <td style="text-align: center;" width="100%">
                 <h2>APLIKASI SELEKSI PENERIMAAN BEASISWA<BR />
-                MTS AL-ISLAMIYAH "SMIP" 1946<br /></h2>
+                    MTS AL-ISLAMIYAH "SMIP" 1946<br /></h2>
             </td>
             {{-- <td width="15%">
             </td> --}}
@@ -26,9 +27,9 @@
     </table>
     <hr>
     <h3 style="text-align: center">LAPORAN HASIL PENILAIAN BEASISWA KURANG MAMPU
-        
+
     </h3>
-    
+
     <br />
     <table width="100%" border="1" cellpadding="5" cellspacing="0">
         <tr>
@@ -37,32 +38,32 @@
             @foreach (kbkm() as $key => $item)
             <th>{{$item->nama}} (K{{$key+1}})</th>
             @endforeach
-            
+
             <th>Si</th>
             <th>Vi</th>
-          </tr>
+        </tr>
         @php
         $no =1;
         @endphp
         @foreach ($data as $key => $item)
         <tr>
-          <td>A.{{1 + $key}}</td>
-          <td>{{$item->nama}}</td>
-          
-          
-          @foreach (kbkm() as $item_k)
-          <td>
-            {{number_format(nilaibkm($item->id, $item_k->id))}}
-          </td>
-          @endforeach
-          <td>
-            {{$item->si}}
-          </td>
-          <td>
-            
-            {{$item->vi}}
-          </td>
-          
+            <td>A.{{1 + $key}}</td>
+            <td>{{$item->nama}}</td>
+
+
+            @foreach (kbkm() as $item_k)
+            <td>
+                {{number_format(nilaibkm($item->id, $item_k->id))}}
+            </td>
+            @endforeach
+            <td>
+                {{number_format($item->si,3)}}
+            </td>
+            <td>
+
+                {{number_format($item->vi,3)}}
+            </td>
+
         </tr>
         @endforeach
     </table>
